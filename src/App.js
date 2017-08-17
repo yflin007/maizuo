b5ccec4589f0a89a7e74f2a1d4c03f36a8cc0c1f
unknown <471968360@qq.com>
2017-08-17 20:09:03 +0800

    基础框架的搭建

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react'
+import {BrowserRouter,Route}  from 'react-router-dom'
+
+
+
+export  default class App extends Component{
+	
+	   constructor(){
+	   	 super();
+	   	
+	   	
+	   	
+	   	
+	   }
+	
+	  render(){
+	  	
+	  	
+	  	
+	  }
+	
+	
+}
